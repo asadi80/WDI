8b8f2bbec46dfa185ec966851ef63b627fb613b5
asadi80 <a8sadi@gmail.com>
2026-01-12 09:22:08 -0800

    updating image

--- components/EquipmentMap.jsx
+++ components/EquipmentMap.jsx
@@ -55,26 +55,26 @@ export default function WaferPhysicalLocation({
 
   /* Module geometry definitions */
   const MODULES = {
-    LP1: { x: 24, y: 145, w: 107, h: 106, label: "LP 1" },
-    LP2: { x: 24, y: 321, w: 107, h: 106, label: "LP 2" },
-    LP3: { x: 24, y: 497, w: 107, h: 106, label: "LP 3" },
+    LP1: { x: 26, y: 155, w: 107, h: 106, label: "LP 1" },
+    LP2: { x: 26, y: 325, w: 107, h: 106, label: "LP 2" },
+    LP3: { x: 26, y: 493, w: 107, h: 106, label: "LP 3" },
 
-    EFEM: { x: 153, y: 320, w: 108, h: 105, label: "EFEM" },
+    EFEM: { x: 153, y: 325, w: 108, h: 105, label: "EFEM" },
 
     LLST1: { x: 376, y: 387, w: 107, h: 110, label: "LL ST1" },
     LLST2: { x: 376, y: 261, w: 107, h: 110, label: "LL ST2" },
 
-    TMST1: { x: 600, y: 388, w: 103, h: 106, label: "TM ST1" },
-    TMST2: { x: 600, y: 266, w: 103, h: 102, label: "TM ST2" },
+    TMST1: { x: 601, y: 390, w: 104, h: 97, label: "TM ST1" },
+    TMST2: { x: 601, y: 272, w: 103, h: 102, label: "TM ST2" },
 
-    PM1ST1: { x: 726, y: 569, w: 112, h: 102, label: "PM 1 ST1" },
-    PM1ST2: { x: 594, y: 569, w: 112, h: 102, label: "PM 1 ST2" },
+    PM1ST1: { x: 727, y: 563, w: 112, h: 102, label: "PM 1 ST1" },
+    PM1ST2: { x: 596, y: 563, w: 112, h: 102, label: "PM 1 ST2" },
 
-    PM2ST1: { x: 946, y: 260, w: 112, h: 102, label: "PM 2 ST1" },
-    PM2ST2: { x: 946, y: 385, w: 112, h: 108, label: "PM 2 ST2" },
+    PM2ST1: { x: 946, y: 265, w: 112, h: 102, label: "PM 2 ST1" },
+    PM2ST2: { x: 946, y: 388, w: 112, h: 108, label: "PM 2 ST2" },
 
-    PM3ST1: { x: 732, y: 76, w: 112, h: 102, label: "PM 3 ST1" },
-    PM3ST2: { x: 600, y: 76, w: 112, h: 102, label: "PM 3 ST2" },
+    PM3ST1: { x: 735, y: 88, w: 112, h: 102, label: "PM 3 ST1" },
+    PM3ST2: { x: 605, y: 88, w: 112, h: 102, label: "PM 3 ST2" },
   };
 
   function resolveModuleFromChamber(chamber) {
