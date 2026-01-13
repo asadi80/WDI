95af7d62d98c5e6d3f7ced11bdeee8cd02e2532f
asadi80 <a8sadi@gmail.com>
2026-01-13 08:36:31 -0800

    wafer position

--- components/tool-layout/MODULES.js
+++ components/tool-layout/MODULES.js
@@ -5,18 +5,18 @@ export const MODULES = {
 
   EFEM: { x: 153, y: 325, w: 108, h: 105, label: "EFEM" },
 
-  LLST1: { x: 376, y: 387, w: 107, h: 110, label: "LL ST1" },
+  LLST1: { x: 376, y: 385, w: 107, h: 110, label: "LL ST1" },
   LLST2: { x: 376, y: 261, w: 107, h: 110, label: "LL ST2" },
 
-  TMST1: { x: 601, y: 390, w: 104, h: 97, label: "TM ST1" },
-  TMST2: { x: 601, y: 272, w: 103, h: 102, label: "TM ST2" },
+  TMST1: { x: 598, y: 388, w: 104, h: 97, label: "TM ST1" },
+  TMST2: { x: 598, y: 272, w: 103, h: 102, label: "TM ST2" },
 
-  PM1ST1: { x: 727, y: 563, w: 112, h: 102, label: "PM 1 ST1" },
-  PM1ST2: { x: 596, y: 563, w: 112, h: 102, label: "PM 1 ST2" },
+  PM1ST1: { x: 725, y: 560, w: 112, h: 102, label: "PM 1 ST1" },
+  PM1ST2: { x: 595, y: 560, w: 112, h: 102, label: "PM 1 ST2" },
 
-  PM2ST1: { x: 946, y: 265, w: 112, h: 102, label: "PM 2 ST1" },
-  PM2ST2: { x: 946, y: 388, w: 112, h: 108, label: "PM 2 ST2" },
+  PM2ST1: { x: 942, y: 264, w: 112, h: 102, label: "PM 2 ST1" },
+  PM2ST2: { x: 942, y: 385, w: 112, h: 108, label: "PM 2 ST2" },
 
-  PM3ST1: { x: 735, y: 88, w: 112, h: 102, label: "PM 3 ST1" },
-  PM3ST2: { x: 605, y: 88, w: 112, h: 102, label: "PM 3 ST2" },
+  PM3ST2: { x: 732, y: 88, w: 112, h: 102, label: "PM 3 ST1" },
+  PM3ST1: { x: 602, y: 88, w: 112, h: 102, label: "PM 3 ST2" },
 };
